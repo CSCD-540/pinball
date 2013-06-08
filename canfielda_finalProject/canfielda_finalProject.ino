26adad698496b01c9849d629f2542d5242d48a87
Andrew Canfield <Andrew@thePOG.net>
2013-06-08 00:15:16 -0700

    final version

--- canfielda_finalProject/canfielda_finalProject.ino
+++ canfielda_finalProject/canfielda_finalProject.ino
@@ -40,6 +40,7 @@ struct GameState {
   int firstHit;
   int inplay;
   int over;
+  unsigned long highscore;
 };
 
 GameState game;
@@ -49,10 +50,12 @@ int playerCount;
 int curPlayer;
 int curBall;
 int highScore;
-int score[MAX_PLAYERS];
+unsigned long score[MAX_PLAYERS];
 
-int leftBankState;
-int rightBankState;
+int laneState;
+int dropTargetState;
+
+int bonusBumpers;
 
 unsigned char switchState;
 
@@ -63,6 +66,8 @@ void setup() {
   
   clearAllLamps();
   clearAllDisplays();
+  
+  game.highscore = 200000;
   
   Serial.begin(9600);
   Serial.println("\n\nsetup complete...");
@@ -85,8 +90,10 @@ void loop() {
   game.inplay    = FALSE;
   game.over      = FALSE;
   
-  leftBankState  = 0;
-  rightBankState = 0;
+  laneState        = 0;
+  dropTargetState  = 0;
+
+  bonusBumpers    = FALSE;
   
   // enable lamp: game over 
   bally.setLamp(12, 2, TRUE);
@@ -106,7 +113,6 @@ void loop() {
   setCredits();
   
   while (!game.over) {
-    Serial.println(playerCount);
     
     if (game.firstHit && bally.getNextEnterTest() == TEST) {
       Serial.print("test button down...");
@@ -149,8 +155,7 @@ void loop() {
             clearAllDisplays();
             
             setBall();
-            resetTable();
-            
+                        
             // disable game over light
             bally.setLamp(12, 2, FALSE);
             
@@ -165,9 +170,8 @@ void loop() {
           playerCount++;
           
           setCredits();
+          resetTable();
           
-          // enable player light
-          bally.setLamp(14, playerCount, TRUE);
           setDisplay(playerCount, 0);
         }
       }
@@ -179,115 +183,141 @@ void loop() {
     switchState = bally.getDebRedgeRow(2);
     // right drop target d
     if (game.inplay && switchState & COL_0) {
-      rightBankState = (rightBankState | COL_0);
-      
+      Serial.println("right drop target d");
+      dropTargetState = (dropTargetState | COL_0);
       addScore(500);
+      incrementBonus();
     }
 
     // right drop target c
     if (game.inplay && switchState & COL_1) {
-      rightBankState = (rightBankState | COL_1);
+      Serial.println("right drop target c");
+      dropTargetState = (dropTargetState | COL_1);
       addScore(500);
+      incrementBonus();
     }
     
     // right drop target b
     if (game.inplay && switchState & COL_2) {
-      rightBankState = (rightBankState | COL_2);
+      Serial.println("right drop target b");
+      dropTargetState = (dropTargetState | COL_2);
       addScore(500);
+      incrementBonus();
     }
         
     // right drop target a
     if (game.inplay && switchState & COL_3) {
-      rightBankState = (rightBankState | COL_3);
+      Serial.println("right drop target a");
+      dropTargetState = (dropTargetState | COL_3);
       addScore(500);
+      incrementBonus();
     }
     
     // left drop target d
     if (game.inplay && switchState & COL_4) {
-      leftBankState = (leftBankState | COL_0);
+      Serial.println("left drop target d");
+      dropTargetState = (dropTargetState | COL_4);
       addScore(500);
+      incrementBonus();
     }
 
     // left drop target c
     if (game.inplay && switchState & COL_5) {
-      leftBankState = (leftBankState | COL_1);
+      Serial.println("left drop target c");
+      dropTargetState = (dropTargetState | COL_5);
       addScore(500);
+      incrementBonus();
     }
     
     // left drop target b
     if (game.inplay && switchState & COL_6) {
-      leftBankState = (leftBankState | COL_2);
+      Serial.println("left drop target b");
+      dropTargetState = (dropTargetState | COL_6);
       addScore(500);
+      incrementBonus();
     }
     
     
     // left drop target a
     if (game.inplay && switchState & COL_7) {
-      leftBankState = (leftBankState | COL_3);
+      Serial.println("left drop target a");
+      dropTargetState = (dropTargetState | COL_7);
       addScore(500);
+      incrementBonus();
     }
     
-    // reset right bank
-    if (game.inplay && rightBankState == 15) {
+    // reset drop targets
+    if (game.inplay && dropTargetState == 255) {
+      Serial.println("drop target bonus");
       bally.fireSolenoid(7, TRUE, TRUE);
-      rightBankState = 0;
-      addScore(1000);
-    }
-    
-    // reset left bank
-    if (game.inplay && leftBankState == 15) {
       bally.fireSolenoid(3, TRUE, TRUE);
-      leftBankState = 0;
-      addScore(1000);
+      dropTargetState = 0;
+      addScore(50000);
     }
     
     
-    switchState = bally.getDebouncedRow(3);
+    switchState = bally.getRedgeRow(3);
     // right flipper feed lane
     if (game.inplay && switchState & COL_0) {
-      
-      addScore(100);
+      Serial.println("right flipper feed lane");
+      addScore(500);
+      bally.setLamp(7, 2, TRUE);
+      incrementBonus();
     }
 
     // left flipper feed lane
     if (game.inplay && switchState & COL_1) {
-      
-      addScore(100);
+      Serial.println("left flipper feed lane");
+      addScore(500);
+      bally.setLamp(7, 3, TRUE);
+      incrementBonus();
     }
     
     // drop target rebound
     if (game.inplay && switchState & COL_2) {
-
+      Serial.println("drop target rebound");
       addScore(100);
     }
         
     // right b lane
     if (game.inplay && switchState & COL_3) {
-
-      addScore(100);
+      Serial.println("right b lane");
+      bally.setLamp(3, 2, TRUE);
+      laneState = laneState | COL_1;
     }
     
     
     // left a lane
     if (game.inplay && switchState & COL_4) {
-      
-      addScore(100);
+      Serial.println("left a lane");
+      bally.setLamp(3, 3, TRUE);
+      laneState = laneState | COL_0;      
     }
 
     // top b lane
     if (game.inplay && switchState & COL_5) {
-      
-      addScore(100);
+      Serial.println("top b lane");
+      bally.setLamp(3, 2, TRUE);
+      laneState = laneState | COL_1;
     }
     
     // top a lane
     if (game.inplay && switchState & COL_6) {
-
-      addScore(100);
+      Serial.println("top a lane");
+      bally.setLamp(3, 3, TRUE);
+      laneState = laneState | COL_0;
+    }
+    
+    if (laneState == 3) {
+      Serial.println("increment lane state");
+      incrementLaneValue();
+      bally.setLampRow(3, FALSE);
+      laneState = 0;  
     }
     
     // top center kick out / saucer
     if (game.inplay && switchState & COL_7) {
+      Serial.println("saucer");
       bally.fireSolenoid(0, TRUE);
       incrementMultiplier();
       addScore(3000);
@@ -297,84 +327,116 @@ void loop() {
     switchState = bally.getSwitchRow(4);
     // right slingshot
     if (game.inplay && switchState & COL_2) {
+      Serial.println("right slingshot");
       bally.fireSolenoid(11, TRUE);
-      addScore(100);
+      addScore(50);
     }
     
     // right out lane
     if (game.inplay && bally.getRedge(4, 0)) {
-      
-      addScore(100);
+      Serial.println("right out lane");
+      if (bally.getLampRow(7) & COL_3)
+        addScore(50000);
+      else 
+        addScore(1000);
     }
 
     // left out lane
     if (game.inplay && bally.getRedge(4, 1)) {
-      
-      addScore(100);
+      Serial.println("left out lane");
+      if (bally.getLampRow(7) & COL_2)
+        addScore(50000);
+      else
+        addScore(1000);
     }
 
     
     
     // left slingshot
     if (game.inplay && switchState & COL_3) {
+      Serial.println("left slingshot");
       bally.fireSolenoid(13, TRUE);
       addScore(100);
     }
     
     // bottom right pop/thumper bumper
-    if (game.inplay && switchState & COL_4) { 
+    if (game.inplay && switchState & COL_4 && bonusBumpers) {
+        Serial.println("bottom right pop bumper"); 
         bally.fireSolenoid(5, TRUE);
-        addScore(100);
+        bally.fireSolenoid(2, TRUE, TRUE);
+        addScore(1000);
     }
     
     // bottom left pop/thumper bumper
-    if (game.inplay && switchState & COL_5) { 
+    if (game.inplay && switchState & COL_5 && bonusBumpers) { 
+        Serial.println("bottom left pop bumper");
         bally.fireSolenoid(14, TRUE);
+        bally.fireSolenoid(2, TRUE, TRUE);
         addScore(100);
     }
     
     // top right pop/thumper bumper
-    if (game.inplay && switchState & COL_6) { 
+    if (game.inplay && switchState & COL_6) {
+        Serial.println("top right pop bumper"); 
         bally.fireSolenoid(9, TRUE);
+        bally.fireSolenoid(2, TRUE, TRUE);
         addScore(100);
     }
     
     // top left pop/thumper bumper
-    if (game.inplay && switchState & COL_7) { 
+    if (game.inplay && switchState & COL_7) {
+        Serial.println("top left pop bumper"); 
         bally.fireSolenoid(1, TRUE);
+        bally.fireSolenoid(2, TRUE, TRUE);
         addScore(100);
     }
-
-      Serial.print("inplay: ");
-      Serial.println(game.inplay);
       
     // outhole
     if (game.inplay && bally.getDebounced(0, 7)) {
-      
+      Serial.println("outhole");
       if (bally.getLampRow(10) & COL_2 || bally.getLampRow(6) & COL_1) {
-        ; // shoot again if player didn't make any points or if player has an extra ball
+        // shoot again if player didn't make any points or if player has an extra ball
+        
+        Serial.println("shoot again!");
+        // clear free ball
+        bally.setLamp(6, 1, FALSE);
+        
+        
       } else {
-        // TODO: calculate bonuses  
+        // bonus
+        addScore(getBonus() * getMultiplier());
+        laneState        = 0;
+        dropTargetState  = 0;
+        bonusBumpers    = FALSE;
+
+        // reset drop targets
+        bally.fireSolenoid(7, TRUE, TRUE);
+        bally.fireSolenoid(3, TRUE, TRUE);
+        
         if (++curPlayer > playerCount) {
           curPlayer = 0;
-        
+          
+          
+                  
           if (++curBall < (N_BALLS + 1)) {
             setBall();
+            game.firstHit = TRUE;
           } else {
             game.over = TRUE;
           }
         }
+        
+        clearAllLamps();
+        resetTable();        
+
 
       }
       
         
       if (!game.over) {
-        resetTable();
-        
-        game.firstHit = TRUE;
         // eject the ball 
         bally.fireSolenoid(6, TRUE);
-        
+        delay(1000);
       }
     } // end outhole
     
@@ -383,16 +445,39 @@ void loop() {
   } // end while
   
   
+  // high score and free games
+  for (i = 0; i < playerCount; i++) {
+    if (score[i] > 200000) {
+      credits++;
+      bally.fireSolenoid(12, TRUE);
+    }
+    
+    if (score[i] > 340000) {
+      credits++;
+      bally.fireSolenoid(12, TRUE);
+    }
+    
+    if (score[i] > 480000) {
+      credits++;
+      bally.fireSolenoid(12, TRUE);
+    }
+    
+    if (score[i] > game.highscore) {
+      game.highscore = score[i];
+      bally.setLamp(12, 1, TRUE);
+    }
+  }
 
   
-  // TODO: Match and knocker
-
-
+  // Match and knocker
+  if (random(0, 10) == 5) {
+    bally.fireSolenoid(10, TRUE);
+    bally.setLamp(10, 1, TRUE); 
+  }
   
 }
 
-void addScore(int points) {
-  if (game.firstHit) {
+void addScore(unsigned long points) {
     game.inplay   = TRUE;
     game.cabinet  = FALSE;
     game.firstHit = FALSE;
@@ -401,11 +486,115 @@ void addScore(int points) {
     bally.setLamp(10, 0, FALSE);
     // disable lamp: shoot again
     bally.setLamp(10, 2, FALSE);
-  }
+
+  
   
   score[curPlayer] += points;
   setDisplay(curPlayer, score[curPlayer]);
+
+  Serial.print("player: ");
+  Serial.print(curPlayer);
+  Serial.print(" points: ");
+  Serial.print(points);
+  Serial.print(" score: ");
+  Serial.println(score[curPlayer]);
 }
+
+int getBonus() {
+  int bonus;
+  unsigned char lamps[3];
+  
+  bonus = 0;
+  
+  lamps[0] = bally.getLampRow(0);
+  lamps[1] = bally.getLampRow(1);
+  lamps[2] = bally.getLampRow(2);
+
+  if (lamps[0] & COL_0)
+    bonus += 1000;
+  if (lamps[0] & COL_1)
+    bonus += 2000;
+  if (lamps[0] & COL_2)
+    bonus += 3000;
+  if (lamps[0] & COL_3)
+    bonus += 4000;
+  if (lamps[1] & COL_0)
+    bonus += 5000;
+  if (lamps[1] & COL_1)
+    bonus += 6000;
+  if (lamps[1] & COL_2)
+    bonus += 7000;
+  if (lamps[1] & COL_3)
+    bonus += 8000;
+  if (lamps[2] & COL_0)
+    bonus += 9000;
+  if (lamps[2] & COL_1)
+    bonus += 10000;
+  if (lamps[2] & COL_2)
+    bonus += 20000;
+
+  return bonus;
+}
+
+void incrementBonus() {
+  int bonus;
+  
+  // chimes the bell
+  bally.fireSolenoid(4, TRUE);
+  
+  // get bonus BEFORE clearing lights
+  bonus = getBonus();
+
+  if (bonus < 29000)
+    bonus += 1000;
+
+  // clear bonus lamps
+  bally.setLampRow(0, FALSE);
+  bally.setLampRow(1, FALSE);
+  bally.setLampRow(2, FALSE);
+  
+  // set bonus lamps
+  if (bonus >= 10000 && bonus < 20000) {
+    bally.setLamp(2, 1, TRUE);
+    bonus -= 10000;
+  }
+  else if (bonus >= 20000) {
+    bally.setLamp(2, 2, TRUE);
+    bonus -= 20000;
+  }
+  
+  switch (bonus) {
+    case 1000:
+      bally.setLamp(0, 0, TRUE);  
+    break;
+    case 2000:
+      bally.setLamp(0, 1, TRUE);  
+    break;    
+    case 3000:
+      bally.setLamp(0, 2, TRUE);  
+    break;    
+    case 4000:
+      bally.setLamp(0, 3, TRUE);  
+    break;    
+    case 5000:
+      bally.setLamp(1, 0, TRUE);  
+    break;    
+    case 6000:
+      bally.setLamp(1, 1, TRUE);  
+    break;    
+    case 7000:
+      bally.setLamp(1, 2, TRUE);  
+    break;    
+    case 8000:
+      bally.setLamp(1, 3, TRUE);
+    break;
+    case 9000:
+      bally.setLamp(2, 0, TRUE);
+    break;
+  } 
+  
+}
+
 
 int getMultiplier() {
   unsigned char lamps;
@@ -447,7 +636,87 @@ void incrementMultiplier() {
   }
 }
 
+int getLaneValue() {
+  unsigned char lamps[2];
+  
+  lamps[0] = bally.getLampRow(5);
+  lamps[1] = bally.getLampRow(6);
+
+  if (lamps[0] & COL_0)
+    return 1000;
+  if (lamps[0] & COL_1)
+    return 2000;
+  if (lamps[0] & COL_2)
+    return 3000;
+  if (lamps[0] & COL_3)
+    return 4000;
+  if (lamps[1] & COL_0)
+    return 5000;
+  if (lamps[1] & COL_1)
+    return 6000;
+  if (lamps[1] & COL_2)
+    return 7000;
+   
+  return 0; 
+}
+
+
+void incrementLaneValue() {
+  int laneValue;
+  
+  // chime the bell
+  bally.fireSolenoid(12, TRUE);
+
+  // enable bonus bumpers
+  bonusBumpers = TRUE;
+  bally.setLampRow(8, TRUE);
+ 
+  // get lane value before resetting lamps
+  laneValue = getLaneValue();
+  if (laneValue < 7000)
+    laneValue += 1000;
+  
+  Serial.print("lane value: ");
+  Serial.println(laneValue);
+    
+  // clear lamps
+  bally.setLampRow(5, FALSE);  
+  bally.setLampRow(6, FALSE);
+  
+  switch(laneValue) {
+    case 1000:
+      bally.setLamp(5, 0, TRUE); 
+    break;
+    case 2000:
+      bally.setLamp(5, 1, TRUE); 
+    break;
+    case 3000:
+      bally.setLamp(5, 2, TRUE); 
+    break;
+    case 4000:
+      bally.setLamp(5, 3, TRUE); 
+    break;
+    case 5000:
+      bally.setLamp(6, 0, TRUE);
+    break;
+    case 6000:
+      bally.setLamp(6, 0, TRUE);
+      bally.setLamp(6, 1, TRUE);
+    break;
+    case 7000:
+      bally.setLamp(6, 1, TRUE);
+      bally.setLamp(6, 2, TRUE);
+    break;
+  }
+  
+  addScore(laneValue);
+}
+
 void resetTable() {
+  int i;
+  
+  Serial.println("resetTable()");
+  
   // saucer
   // bally.fireSolenoid(0, TRUE); 
   
@@ -493,9 +762,16 @@ void resetTable() {
   
   // disable lamps: 1-4 can play
   bally.setLampRow(13, FALSE);
-          
-  // enable lamps: current player can play
-  bally.setLamp(13, curPlayer, TRUE);
+  
+  // enable lamps: 1-4 can play
+  bally.setLamp(13, playerCount, TRUE);
+  
+  // disable lamps: player up
+  bally.setLampRow(14, FALSE);
+
+  Serial.println(curPlayer);  
+  // enable lamps: player up
+  bally.setLamp(14, curPlayer, TRUE);
             
   // enable light: ball in play
   bally.setLamp(12, 0, TRUE);
@@ -508,6 +784,7 @@ void resetTable() {
   
   // enable light: shoot again
   bally.setLamp(10, 2, TRUE);
+  
 }
 
 void setCredits() {
